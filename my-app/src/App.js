54dc7afcde4d109b31f2ed4b3e4fcb2feab9f032
Himanshu Hemant <himanshuhemant1512@gmail.com>
2021-02-26 00:18:42 +0530

    Navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,15 @@ import {Route,Switch} from 'react-router-dom';
 import About from './About';
 import Error from './Error';
 import Contact from './Contact';
+import Navbar from './Navbar';
    
 const App=()=>{
   return (
     <>
+    <Navbar />
       <Switch>
         <Route exact path='/'component={About} />
-        <Route path='/contact'component={Contact} />
+        <Route exact path='/contact'component={Contact} />
         <Route component={Error}/>
       </Switch>
        
