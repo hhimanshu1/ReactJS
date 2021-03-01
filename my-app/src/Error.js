9a6c0f708419294a6b70ed2ae27f0e69c7004623
Himanshu Hemant <himanshuhemant1512@gmail.com>
2021-03-01 13:17:11 +0530

    404

--- my-app/src/Error.js
+++ my-app/src/Error.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 const Error=()=>{
     return (
         <>
-        <h1>OOps, I am Error Page</h1>
+        <h1>404, Error Page</h1>
+        <p>Sorry, This page is not Found</p>
+        <NavLink to="/">Go Back</NavLink>
         </>
     );
 };
