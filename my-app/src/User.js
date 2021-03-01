e78bfdfbf0a69f412156c69d972b0615fe5a778a
Himanshu Hemant <himanshuhemant1512@gmail.com>
2021-03-01 12:06:19 +0530

    useHistory

--- my-app/src/User.js
+++ my-app/src/User.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useParams} from 'react-router-dom'
+import {useParams, useLocation, useHistory} from 'react-router-dom'
 // const User=({match})=>{
 //     return (
 //         <>
@@ -10,11 +10,19 @@ import {useParams} from 'react-router-dom'
 
 const User=()=>{
     const {fname,lname}=useParams();
+    const location=useLocation();
+    const history=useHistory();
     return (
         <>
         <h1>I am  {fname} {lname}Page</h1>
+        <p>My Current location is {location.pathname}</p>
+        {location.pathname===`/user/himanshu/hemant` ?(
+        <button onClick={()=>history.goBack()}>Click Me</button>    
+            
+        ):null}
+    
         </>
-    )
-}
+    );
+};
 
 export default User;
